67cbdb4f996a4fce45f35a893986f2a8bc019af7
Marcus Vieira <marcus.vieira@elinvar.de>
2019-10-27 18:05:05 +0100

    Update config server configurations

--- src/main/java/tech/marcusvieira/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/tech/marcusvieira/springconfigserver/SpringConfigServerApplication.java
@@ -2,12 +2,10 @@ package tech.marcusvieira.springconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class SpringConfigServerApplication {
 
     public static void main(String[] args) {
